2aba0f55b7a8d2f8e678f110eca8266386998881
Amereth <alek.melnyk@gmail.com>
2023-10-27 23:36:22 +0300

    cors and helmet setup

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,41 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import { connectToMongo, db } from './db/mongo.js'
 import helmet from 'helmet'
+import 'express-async-errors'
+import cors from 'cors'
+import {
+  ClerkExpressRequireAuth,
+  RequireAuthProp,
+  LooseAuthProp,
+} from '@clerk/clerk-sdk-node'
+
+declare global {
+  namespace Express {
+    interface Request extends LooseAuthProp {}
+  }
+}
 
 const app = express()
+
 app.use(helmet())
+app.use(cors({ origin: 'http://localhost:3000' }))
 
 // Declare a route
 app.get('/', async (request, response) => {
+  console.log('app.get ~ request:', request)
+
   const data = await db.collection('posts').find().toArray()
   console.log('data:', data)
   response.send(data)
 })
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.log('ERROR', error)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080')
   connectToMongo()
 })
