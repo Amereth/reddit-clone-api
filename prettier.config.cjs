75c182e123b3c9a4a105d1a62273c302acf5da96
Amereth <alek.melnyk@gmail.com>
2023-11-02 21:15:52 +0200

    prettier import sort setup

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,9 +1,9 @@
-/** @type {import("prettier").Config} */
-const config = {
+module.exports = {
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+  importOrderSeparation: false,
+  importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
 }
-
-module.exports = config
