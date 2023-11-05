5aef00bcd54cb0dc60f0161995445d71625d6edc
Amereth <alek.melnyk@gmail.com>
2023-11-05 14:43:41 +0200

    create post api

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,26 +5,32 @@ import clerkClient, {
   WithAuthProp,
 } from '@clerk/clerk-sdk-node'
 import { Request, Router } from 'express'
+import { pick } from 'rambda'
 import { z } from 'zod'
 import { Collections } from '../db/collections.js'
 import { db } from '../db/mongo.js'
+import { getZodSchemaKeys } from '../utils/getZodSchemaKeys.js'
 import { sanitizeResponse } from '../utils/sanitizeResponse.js'
 
 export const postsRouter = Router()
 
 const post = z.object({
-  title: z.string(),
   userId: z.string(),
+  title: z.string(),
+  body: z.string(),
+  hashtags: z.optional(z.array(z.string())),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
 })
-
 type Post = z.infer<typeof post>
+
+const postBody = post.pick({ title: true, body: true, hashtags: true })
+type PostBody = z.infer<typeof postBody>
 
 postsRouter.get(
   '/posts',
   ClerkExpressWithAuth(),
   async (req: WithAuthProp<Request>, res) => {
-    const user = await clerkClient.users.getUser(req.auth?.userId)
-
     const data = await db.collection<Post>(Collections.Posts).find().toArray()
 
     res.send(sanitizeResponse(data))
@@ -35,13 +41,19 @@ postsRouter.post(
   '/posts',
   ClerkExpressRequireAuth(),
   async (req: RequireAuthProp<Request>, res) => {
-    post.omit({ userId: true }).parse(req.body)
+    postBody.parse(req.body)
 
     const user = await clerkClient.users.getUser(req.auth?.userId)
 
+    const body = pick(getZodSchemaKeys(post), req.body) as PostBody
+
+    const createdAt = new Date()
+
     const response = await db.collection<Post>(Collections.Posts).insertOne({
-      title: req.body.title,
+      ...body,
       userId: user.id,
+      createdAt,
+      updatedAt: createdAt,
     })
 
     res.status(201).send(response)
