49fae03f99dc0d3ff0e3f4effc7bde5c5ab6d062
Amereth <alek.melnyk@gmail.com>
2023-12-03 21:01:16 +0200

    implemented pagination

--- src/routes/posts/getPosts.ts
+++ src/routes/posts/getPosts.ts
@@ -6,38 +6,68 @@ import { db } from '../../db/mongo.js'
 import { sanitizeResponse } from '../../utils/sanitizeResponse.js'
 import { Post } from './types.js'
 
+type Query = {
+  hashtag?: string
+  date?: string
+  page?: string
+  perPage?: string
+}
+
 export const getPosts = async (
-  req: WithAuthProp<Request<{ hashtag?: string; date?: string }>>,
+  req: WithAuthProp<Request<Query>>,
   res: Response,
 ) => {
-  const { hashtag, date } = req.query as { hashtag?: string; date?: string }
+  const { hashtag, date, page = '1', perPage = '3' } = req.query as Query
 
   const lteDate = dayjs(date).add(1, 'day').toDate()
 
   const data = await db
     .collection<Post>(Collections.Posts)
-    .find({
-      hashtags: hashtag ? hashtag : { $exists: true },
-      createdAt: date
-        ? {
-            $gte: new Date(date),
-            $lte: lteDate,
-          }
-        : { $exists: true },
-    })
+    .aggregate<{ data: Post[]; total: { total: number }[] }>([
+      {
+        $facet: {
+          data: [
+            {
+              $match: {
+                hashtags: hashtag ? hashtag : { $exists: true },
+                createdAt: date
+                  ? { $gte: new Date(date), $lte: lteDate }
+                  : { $exists: true },
+              },
+            },
+            { $skip: (Number(page) - 1) * Number(perPage) },
+            { $limit: Number(perPage) },
+          ],
+          total: [{ $count: 'total' }],
+        },
+      },
+    ])
     .toArray()
 
-  const responseData = data.map((post) => ({
-    ...post,
-    likes: {
-      total: post.likes.length,
-      isLiked: post.likes.includes(req.auth?.userId),
-    },
-    dislikes: {
-      total: post.dislikes.length,
-      isLiked: post.dislikes.includes(req.auth?.userId),
-    },
-  }))
+  const first = data[0]
 
-  res.send(sanitizeResponse(responseData))
+  if (!first) {
+    res.send(data)
+    return
+  }
+
+  const responseData = {
+    total: data[0]?.total[0]?.total,
+    perPage: Number(perPage),
+    data: first.data
+      .map((post) => ({
+        ...post,
+        likes: {
+          total: post.likes.length,
+          isLiked: post.likes.includes(req.auth?.userId),
+        },
+        dislikes: {
+          total: post.dislikes.length,
+          isLiked: post.dislikes.includes(req.auth?.userId),
+        },
+      }))
+      .map(sanitizeResponse),
+  }
+
+  res.send(responseData)
 }
