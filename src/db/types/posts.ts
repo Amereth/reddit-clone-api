349f1f4b4fa080dd596ab4f9e6b3e20cf9b3f8e3
Amereth <alek.melnyk@gmail.com>
2024-01-04 23:41:32 +0200

    refactored types

--- src/db/types/posts.ts
+++ src/db/types/posts.ts
@@ -2,20 +2,22 @@ import { z } from 'zod'
 import { author } from './author.js'
 
 export const postComment = z.object({
+  createdAt: z.date(),
+  updatedAt: z.date().optional(),
   body: z
     .string()
     .min(1, 'comment must be at least 1 character long')
     .max(1000, 'comment must be less than 1000 characters long'),
-  createdAt: z.date(),
-  updatedAt: z.date().optional(),
-  likes: z.array(z.string()).default([]),
-  dislikes: z.array(z.string()).default([]),
+  likes: z.number().default(0),
+  dislikes: z.number().default(0),
   author,
 })
 
 export type PostComment = z.infer<typeof postComment>
 
 export const post = z.object({
+  createdAt: z.date(),
+  updatedAt: z.date().optional(),
   title: z
     .string()
     .min(3, 'comment must be at least 3 character long')
@@ -25,13 +27,11 @@ export const post = z.object({
     .min(10, 'comment must be at least 10 character long')
     .max(1000, 'comment must be less than 1000 characters long'),
   hashtags: z.optional(z.array(z.string())),
-  likes: z.array(z.string()).default([]),
-  dislikes: z.array(z.string()).default([]),
+  likes: z.number().default(0),
+  dislikes: z.number().default(0),
   imageUrl: z.string().optional(),
-  author,
-  createdAt: z.date(),
-  updatedAt: z.date().optional(),
   comments: z.array(postComment),
+  author,
 })
 
 export type Post = z.infer<typeof post>
